097c1c52ad33c2b41a6b032b3158e20d9d81371f
Aditya Ricky <aditya.14116142@student.itera.ac.id>
2020-05-16 01:13:13 +0700

    Minor update
    +Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Cart from './components/Cart'
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
                     <Route exact path="/" component={Home}/>
                     <Route path="/cart" component={Cart}/>
                   </Switch>
+                  <Footer/>
              </div>
        </BrowserRouter>
       
