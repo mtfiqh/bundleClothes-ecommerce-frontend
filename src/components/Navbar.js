846964e05408b0a1fff1e9417f8c45f5812b5b6a
Aditya Ricky <aditya.14116142@student.itera.ac.id>
2020-05-17 05:30:13 +0700

    Update Navbar.js
    +Search Bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,21 @@ import { Link } from 'react-router-dom'
                 <div className="container">
                     <Link to="/" className="brand-logo">Bundle Cloth</Link>
                     
-                    <ul className="right">
+                    <ul className="right right hide-on-med-and-down">
                         <li><Link to="/">Shop</Link></li>
-                        <li><Link to="/cart"><i className="material-icons left">shopping_cart</i>My cart</Link></li>
+                        <li><Link to="/cart"><i className="material-icons left prefix">shopping_cart</i>My cart</Link></li>
                         <li><Link to="/Register" className="waves-effect waves-light btn">Sign Up</Link></li>
                         <li><Link to="/Login" className="waves-effect waves-light btn">Login</Link></li>
-
+                        <li><div class="center row">
+                          <div class="col s12 " >
+                            <div class="row" id="topbarsearch">
+                              <div class="input-field col s6 s12 ">
+                                <i class="material-icons prefix">search</i>
+                                <input type="text" placeholder="Search Product.."  />
+                                </div>
+                              </div>
+                            </div>
+                          </div></li> 
                     </ul>
                 </div>
             </nav>
